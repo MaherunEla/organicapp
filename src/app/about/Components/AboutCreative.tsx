7256b84a0041a13c09340932d59a4f5935003073
MaherunEla <meherunela2002@gmail.com>
2023-08-08 16:26:41 +0600

    5

---
+++ src/app/about/Components/AboutCreative.tsx
@@ -0,0 +1,21 @@
+import React from 'react'
+import Image from 'next/image'
+const AboutCreative = () => {
+  return (
+    <div className='container'>
+        <div className='flex '>
+            <div className='w-[800px] h-[761px] relative'>
+                <Image src="/assets/images/about/about.png" fill alt="image"/>
+
+            </div>
+            <div>
+
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default AboutCreative
