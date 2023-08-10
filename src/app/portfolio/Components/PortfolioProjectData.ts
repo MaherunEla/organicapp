df7fd2f8136538305c365d7164ebaf7f8244e71a
MaherunEla <meherunela2002@gmail.com>
2023-08-10 10:58:56 +0600

    67

--- src/app/portfolio/Components/PortfolioProjectData.ts
+++ src/app/portfolio/Components/PortfolioProjectData.ts
@@ -3,5 +3,38 @@ export const PortfolioProjectData = [
         id:1,
         img:"/assets/images/portfolio/img1.png",
         title:"Green & Tasty Lemon",
-    }
+        subTitle:"Fruits"
+    },
+    {
+        id:2,
+        img:"/assets/images/portfolio/img2.png",
+        title:"Organic Carrot",
+        subTitle:"Farmer"
+    },
+    {
+        id:3,
+        img:"/assets/images/portfolio/img3.png",
+        title:"Organic Betel Leaf",
+        subTitle:"Leaf"
+    },
+    
+    {
+        id:4,
+        img:"/assets/images/portfolio/img4.png",
+        title:"Natural Tommato",
+        subTitle:"Fruits"
+    },
+    {
+        id:5,
+        img:"/assets/images/portfolio/img5.png",
+        title:"Black Raspberry",
+        subTitle:"Farmer"
+    },
+    {
+        id:6,
+        img:"/assets/images/portfolio/img6.png",
+        title:"Honey Orange",
+        subTitle:"Farmer"
+    },
+
 ]
